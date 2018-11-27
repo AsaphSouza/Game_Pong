dff72173d5197bf689a5626b5683da2ae6a51de8
AsaphSouza <asaphsouza1605@gmail.com>
2018-11-27 00:27:08 -0200

    initial commit

---
+++ Jogo/main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+local x = display.actualContentWidth 
+local y = display.actualContentHeight
+linha = display.newLine(0,y/2,x,y/2)
+linha.anchorY = 0
+linha.anchorX = 0
