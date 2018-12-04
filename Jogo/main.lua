5aeef899e5499f37ebbf05d72c05729103fae675
ASAPH DA SILVA SOUZA <20171d13gr0020@LABS.GARANHUNS.IFPE>
2018-12-03 22:37:36 -0200

    acelerometro

--- Jogo/main.lua
+++ Jogo/main.lua
@@ -5,21 +5,27 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
-local x = display.actualContentWidth 
-local y = display.actualContentHeight
-linha = display.newLine(0,y/2,x,y/2)
-linha.anchorY = 0
+local physics = require("physics")
+physics.start()
+
+linha = display.newLine(0,display.contentCenterY,display.actualContentWidth,display.contentCenterY)
 linha.anchorX = 0
+linha.anchorY = 0
 
-local rec_1 = display.newRect(0,20,125,20)
-rec_1.x = display.contentCenterX
+local rec_1 = display.newRect(display.contentCenterX,display.actualContentHeight*0.01,125,20)
 
-print(display.actualContentHeight)
-
-local rec_2 = display.newRect(0, y - 20,125,20)
-rec_2.x = display.contentCenterX
-
+local rec_2 = display.newRect(display.contentCenterX, display.actualContentHeight * 0.9,125,20)
 
 local bola = display.newCircle(display.contentCenterX,display.contentCenterY,10)
-bola.anchorX = 0
-bola.anchorY = 0
+
+physics.addBody(bola, {density = 1.0, friction = 0.3, bounce = 0.2})
+physics.addBody(rec_1, "static", {density = 1.0, friction = 0.3, bounce = 2})
+physics.addBody(rec_2, "static", {density = 1.0, friction = 0.3, bounce = 2})
+rec_1.gravityScale = 0
+rec_2.gravityScale = 0
+
+function aplicarAceleracao(event)
+    rec_1.x = rec_1.x + (event.xGravity * 20)
+end
+
+Runtime:addEventListener("accelerometer", aplicarAceleracao) 
